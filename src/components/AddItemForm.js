15ad7583c6ea30f19d910b6aa380e2589a33f082
jffernan <jffernan@comcast.net>
2017-12-28 16:14:17 -0800

    new empty js files for 9 components

---
+++ src/components/AddItemForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+let AddItemForm = ({ onSubmit }) => {
+  let input;
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        onSubmit(input.value)
+        input.value = ''
+      }}>
+        <input ref={node => {
+          input = node
+        }} />
+        <button type="submit">
+          Add Item
+        </button>
+      </form>
+    </div>
+  )
+};
+
+export default AddItemForm;
